501960d489a2faed079be1630aec04ddc81b286e
Evans <150334926+kumievans466@users.noreply.github.com>
2023-11-09 10:18:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import calculate
-
 studentName = input("Enter Student Name: ")
 subjectNames = []
 classScores = []
@@ -9,35 +8,32 @@ percentages = []
 grades = []
 results = []
 
-for i in range(3):  
+for i in range(3):
     subject = input("Enter Subject Name: ")
     classScore = float(input(f"Enter class score for {subject} (out of 200): "))
     examScore = float(input(f"Enter exam score for {subject} (out of 500): "))
-    print(classScore, examScore)
-    
+
     subjectNames.append(subject)
     classScores.append(classScore)
     examScores.append(examScore)
 
-for i in range(3):  
+for i in range(3):
     totalScore = calculate.calculateTotalScore(classScores[i], examScores[i])
     percentage = calculate.calculatePercentage(totalScore)
     grade = calculate.calculateGrade(percentage)
     result = calculate.calculateResult(grade)
 
-    oneDp = "{:.1f}".format(percentage)  
+    oneDp = "{:.1f}".format(percentage)
 
     percentages.append(oneDp)
     grades.append(grade)
     results.append(result)
 
-for i in range(3):  
+for i in range(3):
     print(f"Remarks: {studentName} scored {percentages[i]}% in {subjectNames[i]} and had a grade {grades[i]}. Result: {results[i]}")
 
-
-
-plt.bar(subjectNames, percentages,width=0.4)
-plt.xlabel('Subjects')
-plt.ylabel('Percentage')
-plt.title(f'Percentage for {studentName}')
+plt.bar(subjectNames, [float(i) for i in percentages], width=0.4)
+plt.xlabel('SUBJECTS')
+plt.ylabel('PERCENTAGES')
+plt.title(f'Percentages for {studentName}')
 plt.show()
