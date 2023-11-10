daeff43050ab3685ff451b50cef4f863e1b6bd17
Dboateng2 <149882920+Dboateng2@users.noreply.github.com>
2023-11-10 05:22:07 -0800

    main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ examScores = []
 percentages = []
 grades = []
 results = []
-totalScores = []
 
 for i in range(3):
     subject = input("Enter Subject Name: ")
@@ -17,7 +16,6 @@ for i in range(3):
     subjectNames.append(subject)
     classScores.append(classScore)
     examScores.append(examScore)
-    
 
 for i in range(3):
     totalScore = calculate.calculateTotalScore(classScores[i], examScores[i])
@@ -30,16 +28,31 @@ for i in range(3):
     percentages.append(oneDp)
     grades.append(grade)
     results.append(result)
-    totalScores.append(totalScore)
-
 
 for i in range(3):
     print(f"Remarks: {studentName} scored {percentages[i]}% in {subjectNames[i]} and had a grade {grades[i]}. Result: {results[i]}")
 
-
 plt.bar(subjectNames, [float(i) for i in percentages], width=0.4)
 plt.xlabel('SUBJECTS')
-plt.ylabel('PERCENTAGES')
-plt.title(f'PERCENTAGES FOR {studentName}')
+plt.ylabel('MARKS IN PERCENTAGE')
+plt.title(f'Percentages of {studentName} Marks')
 plt.show()
 
+raw_totals = [calculate.calculateTotalScore(classScores[i], examScores[i]) for i in range(3)]
+
+fig, ax = plt.subplots()
+index = range(len(subjectNames))
+bar_width = 0.2
+
+bar1 = plt.bar(index, classScores, bar_width, label='Class Score', color='b')
+
+bar2 = plt.bar([p + bar_width for p in index], examScores, bar_width, label='Exam Score', color='g')
+bar3 = plt.bar([p + bar_width*2 for p in index], raw_totals, bar_width, label='Raw Total', color='r')
+
+plt.xlabel('Subjects')
+plt.ylabel('Scores')
+plt.title(f'Scores for {studentName}')
+plt.xticks([p + bar_width for p in index], subjectNames)
+plt.legend()
+
+plt.show()
